79c19942f37940cbcedacaea881a270c8934a368
StarGrim33 <prosky33@gmail.com>
2022-11-30 14:12:09 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HomeWorkOOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(100, 50, 10, 30);
+            player.ShowStats();
+        }
+    }
+
+    class Player
+    {
+        public Player(int health, int armor, int speed, int damage)
+        {
+            Health = health;
+            Armor = armor;
+            Speed = speed;
+            Damage = damage;
+        }
+
+        private int Health;
+        private int Armor;
+        private int Speed;
+        private int Damage;
+
+        public void ShowStats()
+        {
+            Console.WriteLine("Здоровье: " + Health + "\nБроня: " + Armor + "\nСкорость передивижения: " + Speed + "\nУрон: " + Damage);
+        }
+    }
+}
